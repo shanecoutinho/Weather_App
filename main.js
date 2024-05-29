8b562f42cdbc6c0b16ff8d95ad1e77592a86e492
Shane Coutinho <coutinhoshane7@gmail.com>
2024-05-29 19:21:23 +0200

    Design error msg & add code that checks 404 error

--- main.js
+++ main.js
@@ -9,29 +9,37 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city +`&appid=${apiKey}`)
-    var data = await response.json();
+    if (response.status ===404){
+        document.querySelector(".weather").style.display = "none";
+        document.querySelector(".error").style.display = "block";
+    }else{
+        var data = await response.json();
     
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    
+        if (data.weather[0].main === "Clouds") {
+            weatherIcon.src = "./Images/clouds.png";
+        } else if (data.weather[0].main === "Clear") {
+            weatherIcon.src = "./Images/clear.png";
+        }else if (data.weather[0].main === "Rain") {
+            weatherIcon.src = "./Images/rain.png";
+        }else if (data.weather[0].main === "Drizzle") {
+            weatherIcon.src = "./Images/drizzle.png";
+        }else if (data.weather[0].main === "Mist") {
+            weatherIcon.src = "./Images/mist.png";
+        }else if (data.weather[0].main === "Clouds") {
+            weatherIcon.src = "./Images/clouds.png";
+        }  
+    
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+    
 
-    if (data.weather[0].main === "Clouds") {
-        weatherIcon.src = "./Images/clouds.png";
-    } else if (data.weather[0].main === "Clear") {
-        weatherIcon.src = "./Images/clear.png";
-    }else if (data.weather[0].main === "Rain") {
-        weatherIcon.src = "./Images/rain.png";
-    }else if (data.weather[0].main === "Drizzle") {
-        weatherIcon.src = "./Images/drizzle.png";
-    }else if (data.weather[0].main === "Mist") {
-        weatherIcon.src = "./Images/mist.png";
-    }else if (data.weather[0].main === "Clouds") {
-        weatherIcon.src = "./Images/clouds.png";
-    }  
-
-    document.querySelector(".weather").style.display = "block";
-
+    }
+   
 }
 
 searchBtn.addEventListener("click",()=>{
