bb178c612799e17b9ee10d781aab67d37281cfe1
Shane Coutinho <coutinhoshane7@gmail.com>
2024-05-29 18:59:49 +0200

    Used DOM elements to fetch HTML elements

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 
 const apiKey = "6bd42c22da09e48cd0252893bb8a351a";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=manchester";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
-async function checkWeather() {
-    const response = await fetch(apiUrl + `&appid=${apiKey}`)
-    var data = await response.json();
-    
-    console.log(data);
-    document.querySelector(".").innerHTML = ;
-}
-checkWeather();    
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon")
